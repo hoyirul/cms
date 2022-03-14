283224a63a68aebef82c4acde943247a9b27a7cd
Mochammad Hairullah <irulgraspama621@gmail.com>
2022-03-14 08:34:50 +0700

    Change Controller and View /home

--- app/Http/Controllers/socks/HomeController.php
+++ app/Http/Controllers/socks/HomeController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\socks;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Testmonial;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('app.home.index');
+        $product = Product::all();
+        $testmonial = Testmonial::all();
+        return view('app.home.index', compact('product', 'testmonial'));
     }
 }
--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -90,48 +90,17 @@
 <div class="clothes_main section ">
   <div class="container">
     <div class="row">
-      <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-        <div class="sport_product">
-          <figure><img src="images/basketball.png" alt="img"/></figure>
-          <h3> $<strong class="price_text">50</strong></h3>
-          <h4>basket ball</h4>
+      
+      @foreach ($product as $row) 
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+          <div class="sport_product">
+            <figure><img src="uploads/products/{{ $row->img }}" alt="img"/></figure>
+            <h3> $<strong class="price_text">{{ $row->price }}</strong></h3>
+            <h4>{{ $row->product_name }}</h4>
+          </div>
         </div>
-      </div>
-      <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 ">
-        <div class="sport_product">
-          <figure><img src="images/t-shirt.png" alt="img"/></figure>
-          <h3> $<strong class="price_text">50</strong></h3>
-          <h4> T-Shirt</h4>
-        </div>
-      </div>
-      <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 ">
-        <div class="sport_product">
-          <figure><img src="images/game.png" alt="img"/></figure>
-          <h3> $<strong class="price_text">50</strong></h3>
-          <h4>Game</h4>
-        </div>
-      </div>
-      <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-        <div class="sport_product">
-          <figure><img src="images/basketball.png" alt="img"/"></figure>
-          <h3> $<strong class="price_text">50</strong></h3>
-          <h4>basket ball</h4>
-        </div>
-      </div>
-      <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 ">
-        <div class="sport_product">
-          <figure><img src="images/t-shirt.png" alt="img"/"></figure>
-          <h3> $<strong class="price_text">50</strong></h3>
-          <h4>T-Shirt</h4>
-        </div>
-      </div>
-      <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 ">
-        <div class="sport_product">
-          <figure><img src="images/game.png" alt="img"/"></figure>
-          <h3> $<strong class="price_text">50</strong></h3>
-          <h4>Game</h4>
-        </div>
-      </div>
+      @endforeach
+
     </div>
   </div>
 </div>
@@ -249,36 +218,20 @@
           </ul>
           <!-- The slideshow -->
           <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="titlepage">
-                <div class="john">
-                  <div class="john_image"><img src="images/john-image.png" style="max-width: 100%;"></div>
-                  <div class="john_text">JOHN DUE<span style="color: #fffcf4;">(ceo)</span></div>
-                  <p class="lorem_ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, asIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as </p>
-                  <div class="icon_image"><img src="images/icon-1.png"></div>
+
+            @foreach ($testmonial as $row)
+              <div class="carousel-item {{ ($row->id == 1) ? 'active' : '' }}">
+                <div class="titlepage">
+                  <div class="john">
+                    <div class="john_image"><img src="uploads/testmonials/{{ $row->img_profile }}" style="max-width: 100%;"></div>
+                    <div class="john_text">{{ $row->name }}<span style="color: #fffcf4;">({{ $row->position }})</span></div>
+                    <p class="lorem_ipsum_text">{{ $row->testmonial }}</p>
+                    <div class="icon_image"><img src="images/icon-1.png"></div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="carousel-item">
-              <div class="titlepage">
-                <div class="john">
-                  <div class="john_image"><img src="images/john-image.png" style="max-width: 100%;"></div>
-                  <div class="john_text">JOHN DUE<span style="color: #fffcf4;">(ceo)</span></div>
-                  <p class="lorem_ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, asIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as </p>
-                  <div class="icon_image"><img src="images/icon-1.png"></div>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div class="titlepage">
-                <div class="john">
-                  <div class="john_image"><img src="images/john-image.png" style="max-width: 100%;"></div>
-                  <div class="john_text">JOHN DUE<span style="color: #fffcf4;">(ceo)</span></div>
-                  <p class="lorem_ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, asIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as </p>
-                  <div class="icon_image"><img src="images/icon-1.png"></div>
-                </div>
-              </div>
-            </div>
+            @endforeach
+            
           </div>
           <!-- Left and right controls -->
           <a class="carousel-control-prev" href="#testimonial" data-slide="prev">
